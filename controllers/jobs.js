0e4fd47df3101228979b9ecb10d2f2dfdea31b6e
Tristan White <113216091+up2code3@users.noreply.github.com>
2024-08-25 19:52:51 -0400

    week10 homework

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,30 +1,74 @@
-const { StatusCodes } = require('http-status-codes')
 const Job = require('../models/Job')
+const { StatusCodes } = require('http-status-codes')
+const { BadRequestError, NotFoundError } = require('../errors')
+// const { count } = require('console')
 
 
 
-const getAllJobs = async (req,res) => {
-    res.send('get All Jobs')
+const getAllJobs = async (req, res) => {
+    const jobs = await Job.find({ createdBy: req.user.userId }).sort('createdAt')
+    res.status(StatusCodes.OK).json({ jobs, count: jobs.length })
 }
+
 const getJob = async (req,res) => {
-    res.send('get Job')
+    const {user:{userId},params:{id:jobId}} = req 
+
+    const job = await Job.findOne({
+        _id:jobId,
+        createdBy:userId
+    })
+
+    if(!job){
+        throw new NotFoundError(`${jobId} does not exist`)
+    }
+
+    res.status(StatusCodes.OK).json({job})
 }
+
 const createJob = async (req,res) => {
     req.body.createdBy = req.user.userId
     const job = await Job.create(req.body)
     res.status(StatusCodes.CREATED).json({job})
 }
 
-
-
-
-
-
 const updateJob = async (req,res) => {
-    res.send('update Job')
+    const {
+        body:{company,position},
+        user:{userId},
+        params:{id:jobId},
+    } = req 
+
+    if(company === "" || position === "") {
+        throw new BadRequestError('Fields can not be left')
+    }
+    const job = await Job.findOneAndUpdate(
+        {_id:jobId,createdBy:userId},
+        req.body,
+        {new:true, runValidators:true},
+    )
+    if(!job){
+        throw new NotFoundError(`${jobId} does not exist`)
+    }
+
+    res.status(StatusCodes.OK).json({job})
+    
 }
-const deleteJob = async (req,res) => {
-    res.send('delete Job')
+
+const deleteJob = async (req, res) => {
+    const {
+        user:{userId},
+        params:{id:jobId}
+    } = req 
+
+    const job = await Job.findOneAndDelete({
+        _id:jobId,
+        createdBy:userId,
+    })
+    if(!job){
+        throw new NotFoundError(`${jobId} does not exist`)
+    }
+
+  res.status(StatusCodes.OK).send('Item Deleted')
 }
 
 module.exports = {
--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,24 +1,23 @@
-const User = require('../models/User')
-const jwt = require('jsonwebtoken')
-const  UnauthenticatedError  = require('../errors/unauthenticated')
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const { UnauthenticatedError } = require("../errors");
 
-const auth = async (req,res,next) => {
-//check header
-const authHeader = req.headers.authorization
-if(!authHeader || !authHeader.startsWith('Bearer')){
-    throw new UnauthenticatedError('authentication invalid')
-} 
-const token = authHeader.split(' ')[1]
+const auth = async (req, res, next) => {
+  //check header
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer")) {
+    throw new UnauthenticatedError("authentication invalid");
+  }
+  const token = authHeader.split(" ")[1];
 
-try {
-const payload = jwt.verify(token, process.env.JWT_SECRET)
-    
-req.user = { userId: payload.userId, name: payload.name }
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = { userId: payload.userId, name: payload.name };
 
-next()
-} catch (error) {
-    throw new UnauthenticatedError('Authentication invalid!!!')
-}
-}
+    next();
+  } catch (error) {
+    throw new UnauthenticatedError("Authentication invalid!!!");
+  }
+};
 
-module.exports = auth
+module.exports = auth;
